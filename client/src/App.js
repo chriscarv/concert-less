1e15a77abd9ceb1a55ad79ce7c6ac90600e79baf
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-23 16:31:06 -0400

    files moved to client directory

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import NavBar from './components/Nav';
-
+import HomePage from './components/HomePage';
 function App() {
   return (
+    <div>
     <NavBar></NavBar>
+    <HomePage></HomePage>
+    </div>
   );
 }
 
---
+++ client/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import photo from '../images/osc.png';
+function HomePage() {
+
+    return(
+        <div>
+            <img class="jumbotron" src={photo} alt="osc"></img>
+        </div>
+    )
+}
+
+export default HomePage;
