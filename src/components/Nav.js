99954174625e27b9ab9e82e5b87bc606ddd16e10
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-22 20:06:11 -0400

    initial commit

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <header>
+            <h2>Tickets</h2>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">login</a>
+                        </li>
+                        <li>
+                            <a href="/">signup</a>
+                        </li>
+                    </ul>
+                </nav> 
+        </header>
+    )
+}
+
+export default Nav;
