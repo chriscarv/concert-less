d46e9a3d86bcf27e5390655bcfc577ebea9098c2
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-29 18:16:09 -0400

    events page structure and css

--- client/src/components/EventsPage.js
+++ client/src/components/EventsPage.js
@@ -2,7 +2,24 @@ import React from 'react';
 
 function EventsPage(){
     return(
+     <>
         <h1>Events page</h1>
+        <div className='col'>
+            <div className='card'>
+                <img className='flyer' src={require('../images/bv9-1.jpeg')} alt='broken vow flyer' />
+                <p className='event '>Broken Vow</p>
+                <p className='event '>9-01</p>
+                <button className='buy'>Buy Tickets</button>
+            </div>
+            <div className='card'>
+                <img className='flyer' src={require('../images/ysd9-1jpeg.jpeg')} alt='your spirit dies flyer' />
+                <p className='event '>Your Spirit Dies</p>
+                <p className='event'>9-01</p>
+                <button className='buy'>Buy Tickets</button>
+            </div>
+
+        </div>
+      </>   
     )
 }
 
