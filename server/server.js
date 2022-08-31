bdadb75bb549128e37610ba99cb394f82cf4fc60
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-30 21:34:32 -0400

    serve html

--- server/server.js
+++ server/server.js
@@ -21,14 +21,14 @@ const server = new ApolloServer({
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build, index.html')));
 }
 
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
 
-  
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
