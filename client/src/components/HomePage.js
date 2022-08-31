81a2cf964cf660479ee32d9c147f8594e1291ea4
christian <100375218+chriscarv@users.noreply.github.com>
2022-08-31 14:35:14 -0400

    forms created

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import photo from '../images/main.jpeg';
+import { Form, Button } from 'react-bootstrap';
 function HomePage() {
 
     return(
@@ -7,6 +8,37 @@ function HomePage() {
             
             <img className="jumbotron" src={photo} alt="osc"></img>
             <p>tickets</p>
+            <Form className='form'>
+                <div className='formtext'>Login</div>
+                <Form.Group className='mb-3'>
+                    <Form.Label className='formtext' htmlFor='email'>Email </Form.Label>
+                    <Form.Control name='email' placeholder='your email'></Form.Control>
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                    <Form.Label className='formtext' htmlFor='password'>Password </Form.Label>
+                    <Form.Control type='text' name='password' placeholder='your password'></Form.Control>
+                </Form.Group>
+                <Button
+                >
+                    Submit
+                </Button>
+                <div className='formtext'>Sign up</div>
+                <Form.Group className='mb-3'>
+                <Form.Label className='formtext' htmlFor='username'>Username </Form.Label>
+                <Form.Control type='text' name='username' placeholder='your username'></Form.Control>
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                    <Form.Label className='formtext' htmlFor='email'>Email </Form.Label>
+                    <Form.Control type='text' name='email' placeholder='your email'></Form.Control>
+                </Form.Group>
+                <Form.Group className='mb-3'>
+                    <Form.Label className='formtext' htmlFor='password'>Password </Form.Label>
+                    <Form.Control type='text' name='password' placeholder='your password'></Form.Control>
+                </Form.Group>
+                <Button>
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
